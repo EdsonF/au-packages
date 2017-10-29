6018ccf110f32d1d369e51d21ffecdf4f3095c4e
Peter Keuter <github@peterkeuter.nl>
2017-10-29 22:36:57 +0100

    use different tools dir [AU faas-cli]

--- faas-cli/tools/chocolateyInstall.ps1
+++ faas-cli/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $url          = 'https://github.com/openfaas/faas-cli/releases/download/0.4.20/f
 $checksum     = '2794dc4478ab4f790bc49634a1b98933bdd1feed00beab0216124f6c7eee8b8b'
 $checksumType = 'sha256'
 
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir       = "$env:ChocolateyInstall\lib\$packageName\tools\"
 $fileFullPath   = "$toolsDir\faas-cli.exe"
 
 
